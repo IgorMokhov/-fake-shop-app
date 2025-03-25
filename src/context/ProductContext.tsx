e1c55864ae1dae10c500451bee66e81fd73f597f
Igor Mokhov <igormokhovid@gmail.com>
2025-03-25 16:50:47 +0700

    feat: added the ability to add and remove items from the order

--- src/context/ProductContext.tsx
+++ src/context/ProductContext.tsx
@@ -1,15 +1,46 @@
 import { createContext, ReactNode, useEffect, useState } from 'react';
-import { IProduct } from '../types/products';
+import { ICartItem, IProduct } from '../types/products';
 import { getAllProducts } from '../services/api';
 
 interface IProductContext {
   products: IProduct[];
+  order: ICartItem[];
+  addToOrder: (product: ICartItem) => void;
+  removeFromOrder: (id: number) => void;
 }
 
 export const ProductContext = createContext<IProductContext | null>(null);
 
 export const ProductProvider = ({ children }: { children: ReactNode }) => {
   const [products, setProducts] = useState<IProduct[]>([]);
+  const [order, setOrder] = useState<ICartItem[]>([]);
+
+  const addToOrder = (product: ICartItem) => {
+    setOrder((prevOrder) => {
+      const isExist = prevOrder.some((item) => item.id === product.id);
+
+      if (isExist) {
+        return prevOrder.map((item) =>
+          item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+        );
+      } else {
+        return [
+          ...prevOrder,
+          {
+            id: product.id,
+            title: product.title,
+            price: product.price,
+            image: product.image,
+            quantity: 1,
+          },
+        ];
+      }
+    });
+  };
+
+  const removeFromOrder = (id: number) => {
+    setOrder((prevOrder) => prevOrder.filter((item) => item.id !== id));
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,5 +58,9 @@ export const ProductProvider = ({ children }: { children: ReactNode }) => {
     fetchData();
   }, []);
 
-  return <ProductContext.Provider value={{ products }}>{children}</ProductContext.Provider>;
+  return (
+    <ProductContext.Provider value={{ products, order, addToOrder, removeFromOrder }}>
+      {children}
+    </ProductContext.Provider>
+  );
 };
