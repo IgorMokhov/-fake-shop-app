aada55bc4396a8be11c298a54ffa0e13552a4656
Igor Mokhov <igormokhovid@gmail.com>
2025-04-10 16:33:26 +0700

    feat: added selectedCategories and handleCategoryChange to the FiltersContext

--- src/context/FiltersContext.tsx
+++ src/context/FiltersContext.tsx
@@ -2,19 +2,23 @@ import React, { ChangeEvent, createContext, useState } from 'react';
 
 interface IFiltersContext {
   isOpen: boolean;
-  toggleSidebar: (newOpen: boolean) => void;
   minPrice: number | null;
   maxPrice: number | null;
+  selectedCategories: string[];
+  toggleSidebar: (newOpen: boolean) => void;
   handleMinPriceChange: (e: ChangeEvent<HTMLInputElement>) => void;
   handleMaxPriceChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleCategoryChange: (e: ChangeEvent<HTMLInputElement>, category: string) => void;
 }
 
 export const FiltersContext = createContext<IFiltersContext | null>(null);
 
 export const FiltersProvider = ({ children }: { children: React.ReactNode }) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState<boolean>(true);
   const [minPrice, setMinPrice] = useState<number | null>(null);
   const [maxPrice, setMaxPrice] = useState<number | null>(null);
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+  console.log(selectedCategories);
 
   const toggleSidebar = (newOpen: boolean) => {
     setIsOpen(newOpen);
@@ -30,15 +34,28 @@ export const FiltersProvider = ({ children }: { children: React.ReactNode }) => 
     setMaxPrice(value === '' ? null : Number(value));
   };
 
+  const handleCategoryChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    category: string
+  ) => {
+    if (e.target.checked) {
+      setSelectedCategories((prev) => [...prev, category]);
+    } else {
+      setSelectedCategories((prev) => prev.filter((item) => item !== category));
+    }
+  };
+
   return (
     <FiltersContext.Provider
       value={{
         isOpen,
-        toggleSidebar,
         minPrice,
         maxPrice,
+        selectedCategories,
+        toggleSidebar,
         handleMinPriceChange,
         handleMaxPriceChange,
+        handleCategoryChange,
       }}
     >
       {children}
