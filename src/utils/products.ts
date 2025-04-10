7b2eb9941b66532b1e03fd0267147656b140b08f
Igor Mokhov <igormokhovid@gmail.com>
2025-04-10 14:26:43 +0700

    feat: added util getProductsByFilters

--- src/utils/products.ts
+++ src/utils/products.ts
@@ -2,5 +2,21 @@ import { IProduct } from '../types/products';
 
 export const filterProductsByTitle = (search: string, products: IProduct[]) => {
   if (!search) return products;
-  return products.filter(({ title }) => title.toLowerCase().includes(search.toLowerCase()));
+  return products.filter(({ title }) =>
+    title.toLowerCase().includes(search.toLowerCase())
+  );
+};
+
+export const getProductsByFilters = (
+  minPrice: number | null,
+  maxPrice: number | null,
+  products: IProduct[]
+) => {
+  const filteredProductsByPrice = products.filter(({ price }) => {
+    const aboveMin = minPrice !== null ? price >= minPrice : true;
+    const belowMax = maxPrice !== null ? price <= maxPrice : true;
+    return aboveMin && belowMax;
+  });
+
+  return filteredProductsByPrice;
 };
