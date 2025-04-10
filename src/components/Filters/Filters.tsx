c7e5ff449d198051eb4056fa5f8bf62115013a5c
Igor Mokhov <igormokhovid@gmail.com>
2025-04-10 16:38:33 +0700

    feat: added checkboxes for categories in the Filters component

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,9 +1,22 @@
-import { Box, FormGroup, TextField, Typography } from '@mui/material';
+import {
+  Box,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { useFilters } from '../../hooks/useFilters';
 
 export const Filters = () => {
-  const { minPrice, maxPrice, handleMinPriceChange, handleMaxPriceChange } =
-    useFilters();
+  const {
+    minPrice,
+    maxPrice,
+    selectedCategories,
+    handleMinPriceChange,
+    handleMaxPriceChange,
+    handleCategoryChange,
+  } = useFilters();
 
   return (
     <FormGroup>
@@ -26,6 +39,53 @@ export const Filters = () => {
           label="Maximum price"
           variant="outlined"
           type="number"
+        />
+      </Box>
+
+      <Typography sx={{ pt: 4 }} variant="h6" component="p">
+        Category
+      </Typography>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <FormControlLabel
+          control={
+            <Checkbox
+              name="mensClothing"
+              checked={selectedCategories.includes('mensClothing')}
+              onChange={(e) => handleCategoryChange(e, 'mensClothing')}
+            />
+          }
+          label="Men's clothing"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              name="womensClothing"
+              checked={selectedCategories.includes('womensClothing')}
+              onChange={(e) => handleCategoryChange(e, 'womensClothing')}
+            />
+          }
+          label="Women's clothing"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              name="electronics"
+              checked={selectedCategories.includes('electronics')}
+              onChange={(e) => handleCategoryChange(e, 'electronics')}
+            />
+          }
+          label="Electronics"
+        />
+
+        <FormControlLabel
+          control={
+            <Checkbox
+              name="jewelery"
+              checked={selectedCategories.includes('jewelery')}
+              onChange={(e) => handleCategoryChange(e, 'jewelery')}
+            />
+          }
+          label="Jewelery"
         />
       </Box>
     </FormGroup>
